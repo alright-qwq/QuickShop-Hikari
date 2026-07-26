bcc549e880057dd283aff40fa784e8f7427c32d7
ztrnb666 <ztr20121@outlook.com>
2026-07-26 19:15:54 +0800

    feat(exchange): scaffold addon module

---
+++ addon/exchange/src/main/java/com/ghostchu/quickshop/addon/exchange/Main.java
@@ -0,0 +1,10 @@
+package com.ghostchu.quickshop.addon.exchange;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+  @Override
+  public void onEnable() {
+    saveDefaultConfig();
+  }
+}
---
+++ addon/exchange/src/test/java/com/ghostchu/quickshop/addon/exchange/ModuleSmokeTest.java
@@ -0,0 +1,13 @@
+package com.ghostchu.quickshop.addon.exchange;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ModuleSmokeTest {
+  @Test
+  void exposesAddonMainClass() {
+    assertThat(Main.class.getName())
+        .isEqualTo("com.ghostchu.quickshop.addon.exchange.Main");
+  }
+}
